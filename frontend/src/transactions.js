4cab93bbef4b38c077566e5afa6261624e0c048c
thomas dubiel <github email address>
2020-07-29 08:54:15 -0500

    transaction constructor method

--- frontend/src/transactions.js
+++ frontend/src/transactions.js
@@ -1,1 +1,9 @@
-console.log("testing");
+class Transaction {
+  constructor(amount, transaction_type, institution) {
+    this.amount = amount;
+    this.transaction_type = transaction_type;
+    this.institution = institution
+  }
+
+  //render transaction instance method that renders transaction
+}
